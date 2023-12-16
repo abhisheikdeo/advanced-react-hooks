71777bdaec8c9058885548627e0bf662a895139e
Abhisheik Deo <adeo@Abhis-M1-Max-MBP.local>
2023-12-16 18:20:49 +0530

    feat: Logic into Hook and formatting code.

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -6,18 +6,18 @@ import * as React from 'react'
 const countReducer = (state, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return {count: state.count + action.step}
+      return { count: state.count + action.step }
     default:
       throw new Error('Unknown action type:' + action.type)
   }
 }
 
-function Counter({initialCount = 0, step = 1}) {
+function Counter({ initialCount = 0, step = 1 }) {
   const [ state, dispatch ] = React.useReducer(countReducer, {
     count: initialCount,
   })
-  const {count} = state
-  const increment = () => dispatch({type: 'INCREMENT', step})
+  const { count } = state
+  const increment = () => dispatch({ type: 'INCREMENT', step })
   return <button onClick={increment}>{count}</button>
 }
 
